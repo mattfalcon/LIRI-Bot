5933d4412907508e3fd4d0754baa772a1eaf3841
Matt Valladarez <mattvalladarez@gmail.com>
2017-05-12 09:34:18 -0500

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: '	J0xrAxe9V2jlSTVsWn2dnuZTn',
+  consumer_secret: 'fBW2bK880AlqOmoVwKG9hHxTlgTQ6Xyy4JqXY6C4iGU03qK8xL',
+  access_token_key: 	'863018910177779714-xeq77dOFMZ8OwYtghBxheW21eF5xWKP',
+  access_token_secret: 'vuUJ1iZTAppYTKM5DKdYQe1SjMGZRaiKiOZ3aAuR1Yt1n',
 }

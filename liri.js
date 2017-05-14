5f18fc786b44c2d53dc4001b683ab00d5665685c
Matt Valladarez <mattvalladarez@gmail.com>
2017-05-14 18:29:05 -0500

    Update liri.js

--- liri.js
+++ liri.js
@@ -1,7 +1,28 @@
-// Code you need to grab the data from keys.js
+console.log("the bot is starting");
 
-//Liri can take in one of the following commands
-//my-tweets
-//spotify-this-song
-//movie-this
-//do-what-it-says
+var Twit = require('twit');
+var fs = require('fs');
+
+var config = require('./keys')
+console.log('keys')
+
+var T = new Twit(config);
+
+//==============GET==========================
+//===========functionality search/tweets ==============
+var params = {
+    screen_name: "@rangers",
+    count: 20,
+    lang: 'en',  
+}
+
+//================User Input required 'statuses/user_timeline' ============
+T.get('statuses/user_timeline', params, gotData);
+//gotData function to pull data parameter
+function gotData(err, data, response){
+    console.log(data);
+    var tweets = data.statuses;
+    for (var i = 0; i < data.length; i++) {
+    console.log(data[i].text, " ", data[i].created_at);
+    }
+};

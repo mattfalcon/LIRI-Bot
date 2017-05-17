f58b688f3e26f514f30292e0ccfe01398ff2cc06
Matt Valladarez <mattvalladarez@gmail.com>
2017-05-17 18:07:29 -0500

    Update keys.js
    Removed Keys per Stephanie (TA)

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '	J0xrAxe9V2jlSTVsWn2dnuZTn',
-  consumer_secret: 'fBW2bK880AlqOmoVwKG9hHxTlgTQ6Xyy4JqXY6C4iGU03qK8xL',
-  access_token_key: 	'863018910177779714-xeq77dOFMZ8OwYtghBxheW21eF5xWKP',
-  access_token_secret: 'vuUJ1iZTAppYTKM5DKdYQe1SjMGZRaiKiOZ3aAuR1Yt1n',
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: 	'',
+  access_token_secret: '',
 }
